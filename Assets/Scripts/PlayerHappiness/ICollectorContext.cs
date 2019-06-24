22392d1f530b273a26b10ababbba92a2cb085410
Sergey Yanchi <sergeyyanchi@unity3d.com>
2019-06-24 12:27:54 +0300

    rename to media

--- Assets/Scripts/PlayerHappiness/ICollectorContext.cs
+++ Assets/Scripts/PlayerHappiness/ICollectorContext.cs
@@ -3,6 +3,6 @@ namespace PlayerHappiness
     public interface ICollectorContext
     {
         IFrame DoFrame();
-        void SetMetadata(string name, byte[] data);
+        void SetMedia(string name, byte[] data);
     }
 }
