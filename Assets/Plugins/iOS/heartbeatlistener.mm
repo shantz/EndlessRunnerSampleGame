188deab1c3cb725105817a1320d959930575e69c
Üstün Ergenoglu <ustun@unity3d.com>
2019-06-25 17:10:47 +0200

    Fix heart beat connect interval

--- Assets/Plugins/iOS/heartbeatlistener.mm
+++ Assets/Plugins/iOS/heartbeatlistener.mm
@@ -62,7 +62,7 @@
 
         if(error) {
             NSLog(@"ERROR: Failed to create sensor connection! \n%@", error);
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                 [self connect:deviceInformation];
             });
         } else {
